d9b6cdbcfdc376ede1fd125df23bac307e072d49
nurujjamanpollob <nurujjamanpollob@gmail.com>
2025-02-19 18:40:17 +0600

    improve implementation and detection of domain and subdomain

--- user-registration-login/disposable-mail-verify/verify_disposable_mail.php
+++ user-registration-login/disposable-mail-verify/verify_disposable_mail.php
@@ -43,18 +43,6 @@ class DisposableEmailVerifier   {
         return false;
     }
 
-    /**
-     * This method is kind of depending on probability,
-     * and this method used to correctly filter out the domain name from the given address.
-     *
-     * In some challenging cases, the domain name is not the last part of the email address, some examples are:
-     * 1. anonymous.example.com.au (domain name is example.com.au), but we can't just take the last two parts of the address, because it is going to be com.au, and it is not a domain name. but we still need to correctly filter out the domain name, and this method is used to do that.
-     * We can use probability to possibly match names, such as breaking them effectively by the dot, match and count how many identical parts are there, and if the count is more than 1, then we can say that the domain name is the last two parts of the address.
-     * This method is not perfect, but it is a good way to filter out domain names from the given address. at least it is providing better results than just taking the last two parts of the address.
-     * @param string $domain domain name $domain
-     * @param string $another email address $another
-     * @return bool
-     */
     private function deepDomainNameMatch(string $domain, string $another): bool
     {
         // split the domain name by dot
@@ -81,41 +69,54 @@ class DisposableEmailVerifier   {
             return false;
         }
 
-        // count the number of identical parts
-        $identical_parts_count = 0;
-
         // if domain part is bigger than another part, then we need to run the loop by the size of domain part, otherwise, we need to run the loop by the size of another part
         if ($domain_parts_count > $another_parts_count) {
-            for ($i = 0; $i < $domain_parts_count; $i++) {
 
-                // secondary loop to match single part in the array
-                for ($j = 0; $j < $another_parts_count; $j++) {
-                    if (isset($domain_parts[$i]) && isset($another_parts[$j]) && $domain_parts[$i] === $another_parts[$j]) {
-                        $identical_parts_count++;
-                    }
-                }
+            return $this->matchDomainParts($domain_parts, $another_parts);
 
-
-            }
         } else {
-            for ($i = 0; $i < $another_parts_count; $i++) {
 
-                // secondary loop to match single part in the array
-                for ($j = 0; $j < $domain_parts_count; $j++) {
-                    if (isset($domain_parts[$j]) && isset($another_parts[$i]) && $domain_parts[$j] === $another_parts[$i]) {
-                        $identical_parts_count++;
-                    }
-                }
+            return $this->matchDomainParts($another_parts, $domain_parts);
+        }
+
+
+    }
+
+    private function matchDomainParts(array $domainPartsBiggest, array $domainPartsSmallest): bool {
+
+        // if any of part is empty, or less than 2, then return false
+        if(count($domainPartsBiggest) < 2 || count($domainPartsSmallest) < 2) {
+            return false;
+        }
+
+
+        // create a copy array, which can be modified
+        $domainPartsBiggestCopy = $domainPartsBiggest;
+
+        // get the first occurrence of the part in the biggest array
+        $firstOccurrence = array_search($domainPartsSmallest[0], $domainPartsBiggestCopy);
+
+        // check the first occurrence
+        if($firstOccurrence === false) {
+            return false;
+        }
+
+        // slice the biggest array from the first occurrence
+        $domainPartsBiggestCopy = array_slice($domainPartsBiggestCopy, $firstOccurrence);
+
+        // now test if the smaller array not larger than the biggest array
+        if(count($domainPartsSmallest) > count($domainPartsBiggestCopy)) {
+            return false;
+        }
+
+        // now test their equality and order, to match each part, if any part is not matched, then return false
+        foreach($domainPartsSmallest as $key => $part) {
+            if($part !== $domainPartsBiggestCopy[$key]) {
+                return false;
             }
         }
 
-        // if the identical parts count is more than 1, then it is a domain name
-        if ($identical_parts_count > 1) {
-            return true;
-        }
 
-        return false;
-
-
+        return true;
     }
 }
