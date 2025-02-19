c17a6604cff72b29cac110c9bf04b51e48a8cc7a
nurujjamanpollob <nurujjamanpollob@gmail.com>
2025-02-19 12:19:14 +0600

    user creation page out layout not responsive fixed

--- user-registration-login/outputs/user_created_successfully_page_output.php
+++ user-registration-login/outputs/user_created_successfully_page_output.php
@@ -10,63 +10,87 @@ function user_created_successfully_page_output($username, $email)
 
     ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Created Successfully</title>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>User Created Successfully</title>
 
-    <style>
-
-        body {
-            height: 100%;
-            overflow: hidden;
-        }
-        .centered-div {
-            display: flex;
-            height: 100vh;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
-
-        @media (max-width: 1000px) {
-            .success_message_image {
-                height: auto;
-            }
+        <style>
 
             body {
-                margin-left: 30px;
-                margin-right: 30px;
                 height: 100%;
                 overflow: hidden;
             }
 
-            h1 {
-                text-align: center;
+            .centered-div {
+                display: flex;
+                height: 100vh;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
             }
 
-            p {
-                text-align: center;
+            @media (max-width: 1000px) {
+                .success_message_image {
+                    height: auto;
+                }
+
+                body {
+                    margin-left: 30px;
+                    margin-right: 30px;
+                    height: 100%;
+                    overflow: hidden;
+                }
+
+                h1 {
+                    text-align: center;
+                }
+
+                p {
+                    text-align: center;
+                }
             }
-        }
-    </style>
-</head>
+
+            @media (min-width: 1000px) {
+                .success_message_image {
+                    height: 100%;
+                }
+
+                body {
+                    margin-left: 30px;
+                    margin-right: 30px;
+                    height: 100%;
+                    overflow: hidden;
+                }
+
+                h1 {
+                    text-align: center;
+                }
+
+                p {
+                    text-align: center;
+                }
+            }
+        </style>
+    </head>
 
     <body style="font-family: 'DM Sans',sans-serif;">
 
     <div class="centered-div">
 
-        <img class="success_message_image" src="<?php echo $user_created_successfully_image_url; ?>" alt="User Created Successfully" style="max-height: 400px; max-width: 1000px" width="100%" height="70%">
+        <img class="success_message_image" src="<?php echo $user_created_successfully_image_url; ?>"
+             alt="User Created Successfully" style="max-height: 500px; max-width: 1000px" width="100%" height="70%">
 
-        <h1><?php echo $username?>, your user account created successfully!</h1>
+        <h1><?php echo $username ?>, your user account created successfully!</h1>
 
-        <p>Thank you for registering with us. We have emailed you to <?php echo $email?>. Please check your email and set a new password to access this website.</p>
+        <p>Thank you for registering with us. We have emailed you to <?php echo $email ?>. Please check your email and
+            set a new password to access this website.</p>
 
     </div>
     </body>
-</html>
+    </html>
     <?php
 
 }
